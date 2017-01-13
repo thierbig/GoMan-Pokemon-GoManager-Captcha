2f2bc58d57c45bab626141089db77e5541828b28
Chancey <hawbakerchancey@gmail.com>
2017-01-12 21:33:45 -0800

    Updated

--- GoMan-Pokemon-GoManager-Captcha/MainForm.cs
+++ GoMan-Pokemon-GoManager-Captcha/MainForm.cs
@@ -48,7 +48,7 @@ namespace GoManCaptcha
         {
             foreach (var managerHandler in _accounts)
             {
-                if(managerHandler.Manager.AccountState == AccountState.CaptchaRequired || managerHandler.Manager.State == BotState.Running)
+                if(managerHandler.Manager.AccountState == AccountState.CaptchaRequired || managerHandler.Manager.State != BotState.Stopped)
                     objectListView1.RefreshObject(managerHandler);
             }
            
