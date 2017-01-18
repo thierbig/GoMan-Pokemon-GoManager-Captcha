4c7ae6dbef20dfaa27c6ff89eb0d7ae451795bed
Chancey <hawbakerchancey@gmail.com>
2017-01-17 21:54:31 -0800

    UPDATED LINKS TO USE BIT.LY

--- GoMan-Pokemon-GoManager-Captcha/VersionModel.cs
+++ GoMan-Pokemon-GoManager-Captcha/VersionModel.cs
@@ -6,11 +6,11 @@ namespace GoManCaptcha
     class VersionModel
     {
         [JsonIgnore]
-        public static readonly string CurrentVersion = "1.6";
+        public static readonly string CurrentVersion = "1.7";
         [JsonIgnore]
         public static readonly string SavePath = "./Plugins/GoMan-Pokemon-GoManager-Captcha.dll";
         [JsonIgnore]
-        public static readonly Uri Uri = new Uri("https://raw.githubusercontent.com/chancity/GoMan-Pokemon-GoManager-Captcha/master/GoMan-Pokemon-GoManager-Captcha/version.json");
+        public static readonly Uri Uri = new Uri("http://bit.ly/2k3b0PF");
         public string Version { get; set; }
         public string UpdateUrl { get; set; }
     }
