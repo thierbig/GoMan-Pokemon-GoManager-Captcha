4031c17ba3bd11e2746181ce470157906287efd4
Chancey <hawbakerchancey@gmail.com>
2017-02-10 22:56:34 -0800

    initial

---
+++ Goman-Plugin/Model/GlobalSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Goman_Plugin.Model
+{
+    class GlobalSettings
+    {
+        public bool SaveLogs { get; set; } = true;
+        public bool AutoUpdate { get; set; } = true;
+        public bool ToastNotifications { get; set; } = false;
+
+    }
+}
