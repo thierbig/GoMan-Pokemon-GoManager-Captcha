14c53d2735017798bd0168e3d7bbb2555aa09484
Chancey <hawbakerchancey@gmail.com>
2017-02-10 23:00:25 -0800

    READY

--- GoMan-Pokemon-GoManager-Plugin/Model/VersionModel.cs
+++ GoMan-Pokemon-GoManager-Plugin/Model/VersionModel.cs
@@ -6,7 +6,7 @@ namespace GoMan.Model
     class VersionModel
     {
         [JsonIgnore]
-        public static readonly string CurrentVersion = "3.7";
+        public static readonly string CurrentVersion = "3.8";
         [JsonIgnore]
         public static readonly string SavePath = "./Plugins/GoMan-Pokemon-GoManager-Plugin.dll";
         [JsonIgnore]
